72cc9c82f3fe4c3648d76be25b8578e001aff6a1
Audrey Claris <audrey claris>
2020-04-22 18:19:36 +0200

    ewewe

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -1,25 +1,25 @@
 <?php
-
-// src/Controller/LuckyController.php
 namespace App\Controller;
-
 use App\Entity\Categorie;
 use App\Entity\Question;
 use App\Entity\Reponse;
-
-
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\Form\Extension\Csrf\CsrfExtension;
+use Symfony\Component\Form\Forms;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Security\Csrf\CsrfTokenManager;
+use Symfony\Component\Security\Csrf\TokenGenerator\UriSafeTokenGenerator;
+use Symfony\Component\Security\Csrf\TokenStorage\SessionTokenStorage;
 
 class QuizController extends AbstractController
 {
     public function index()
     {
         $quizzs = $this->getDoctrine()->getRepository(Categorie::class)->findAll();
-
-        // var_dump($quizzs);
-
         return $this->render('quiz/index.html.twig', ['quizzs' => $quizzs]);
     }
 
@@ -31,21 +31,54 @@ class QuizController extends AbstractController
         if (!$quiz) {
             return $this->render('quiz/index.html.twig');
         }
-
-        return new Response('Check out this great product: ' . $quiz->getName());
+        return new Response('Check out this great product: ' . $quiz->getName()  . "<a href='/quiz/$id/1'>Commencer le test</a>");
     }
 
-    public function show_question($id, $question_num)
+    public function show_question($id, $question_num, Request $request)
     {
-        $limit = 1;
+        $lastQuestion = $this->getDoctrine()->getRepository(Question::class)->findOneBy(array('idCategorie' => $id), array('id' => 'DESC'));
         $offset = $question_num - 1;
-        $question = $this->getDoctrine()->getRepository(Question::class)->findBy(array("idCategorie" => $id), null, $limit, $offset);
+        $question = $this->getDoctrine()->getRepository(Question::class)->findBy(array("idCategorie" => $id), null, 1, $offset);
         $question = $question[0];
         $reponses = $this->getDoctrine()->getRepository(Reponse::class)->findBy(array("idQuestion" => $question->getId()), null);
+        $session = new Session();
+        $csrfGenerator = new UriSafeTokenGenerator();
+        $csrfStorage = new SessionTokenStorage($session);
+        $csrfManager = new CsrfTokenManager($csrfGenerator, $csrfStorage);
+        $formFactory = Forms::createFormFactoryBuilder()
+            ->addExtension(new CsrfExtension($csrfManager))
+            ->getFormFactory();
+        $form = $formFactory->createBuilder()
+            ->add('reponse', ChoiceType::class, [
+                'choices'  => [
+                    $reponses[0]->getReponse() => 0,
+                    $reponses[1]->getReponse() => 1,
+                    $reponses[2]->getReponse() => 2,
+                ],
+                'expanded' => true,
+                'multiple' => false
+            ])
+            ->add('save', SubmitType::class, ['label' => 'Create Task'])
+            ->getForm();
 
+        if ($request->isMethod('POST')) {
+            $form = $form->submit($request->request->get($form->getName()));
+            $answer = $form['reponse']->getData();
+            if ($form->isSubmitted() && $form->isValid()) {
+                if ($reponses[$answer]->getReponseExpected() == true) {
+                    $this->addFlash('success', 'BONNE REPONSE');
+                } else {
+                    $this->addFlash('error', 'MAUVAISE REPONSE');
+                }
+                if ($question == $lastQuestion) {
+                    return $this->render('results.html.twig');
+                } else {
+                    $next = $question_num + 1;
+                    return $this->render('reponse.html.twig', ['id' => $id, 'next' => $next]);
+                }
+            }
+        }
 
-
-        var_dump($reponses);
-        return new Response("ca c'est l'id :" . $id . " et ca c'est la quesiton numero" .   $question_num . "<br> questionnn" . $question->getQuestion());
+        return $this->render('question.html.twig', ['form' => $form->createView(), 'question' => $question->getQuestion()]);
     }
 }
